47a3f3b02af0579c65a067a579556fde02c6b617
Fayez <faizurrahman.fayez@gmail.com>
2020-10-04 18:35:25 +0600

    added secret_key generator function

--- preinstall.py
+++ preinstall.py
@@ -1,8 +1,15 @@
 import os
+import string
+import random
 import platform
 
 system = (platform.system() == "Linux")
 python = "python3" if system else "python"
+
+char_set = "".join([string.ascii_letters, string.digits, string.punctuation])
+
+with open(f'{os.getcwd()}/mte_system/mte_system/secret_key.key', 'w') as file1:
+    file1.write("".join(random.choice(char_set) for _ in range(50)))
 
 commands = [
     "pip install pylint django" if system else "python -m pip install pylint django",
@@ -14,13 +21,16 @@ commands = [
 ]
 
 for command in commands: 
-
     print(f"executing command: {command}")
-    
-    if command.split(" ")[0] == "dir":
-        os.chdir(command.split(" ")[1])
 
-        continue
+    try:
         
-    os.system(command)
+        if command.split(" ")[0] == "dir":
+            os.chdir(command.split(" ")[1])
 
+            continue
+        
+        os.system(command)
+
+    except KeyboardInterrupt:
+        continue
--- readme.md
+++ readme.md
@@ -1,11 +1,10 @@
-After clonning,
-If virtualenv not installed, install it with: "python3 -m pip install virtualenv"
-Then, create a virtualenv and activate the env. you can follow these command: 
+After clonning, if virtualenv not installed, install it with: "python3 -m pip install virtualenv"
+Then, create a virtual environment and activate that. You can follow these command: 
 
->> virtualenv env_name
+>> virtualenv env_name (for creating the virtual environment)
 >> . ./env_name/bin/activate (if you are on linux) || >> env_name\Scripts\activate.bat (for windows)
 
-after that, run the preinstall file with : "python3 preinstall.py" to complete the installation.
+after that, run the preinstall file with : "python3 preinstall.py" to complete the installation. (make sure your terminal or cmd working directory is in the preinstall file's directory) 
 
 Thanks,
 Fayez
