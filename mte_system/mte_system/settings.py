5e809d7e02eeed11460a244ed27caebcf8bef1f8
faizurrahman1998 <49806769+faizurrahman1998@users.noreply.github.com>
2022-02-01 10:16:55 +0600

    Update settings.py

--- mte_system/mte_system/settings.py
+++ mte_system/mte_system/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'mte_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'fayez', 
+        'HOST': 'localhost', 
+        'PASSWORD': ''
     }
 }
 
